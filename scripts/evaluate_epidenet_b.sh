0b0f042deb3370a0d4a099e16ddf18c2d070283c
Eshank Nazare <eshanknazare@gmail.com>
2024-12-05 17:21:29 +0100

    updated script file

--- scripts/evaluate_epidenet_b.sh
+++ scripts/evaluate_epidenet_b.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 python train.py --evaluate --model epidenet_b \
   --dataset chbmit_singlech_768samples_patient_5_leave_out_seizure_1 \
-  --exp-load-weights-from ./logs/cross_validation/epidenet_v3.1_768samples/2024.10.21-204649-epidenet_b_v3.1_768samples_patient5_leaveout1_lr1e-4_adam_multisteplr150_qat_train/qat_best-q.pth.tar \
+  --exp-load-weights-from ./logs/cross_validation/epidenet_v3.2_768samples_qat8/2024.10.06-095747-epidenet_b_v3.2_768samples_patient5_leaveout1_lr1e-4_adam_multisteplr85_qat_train/qat_best-q.pth.tar \
   --save-sample 9 \
   --confusion -8 --device MAX78000 "$@"
 exec $SHELL
